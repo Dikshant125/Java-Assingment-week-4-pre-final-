8237957bc45d5632288e7db80ea6e065db43f0a7
Dikshant125 <dikshantgoswami441@gmail.com>
2024-09-26 14:46:15 +0530

    Add files via upload
    This is an improved version  of conversions and Arithmetic operations

---
+++ Constants.java
@@ -0,0 +1,60 @@
+/*
+* Here we use the common lines in the form of Constant so we don't need to re-write
+* owner:- Dikshant Goswami
+* DATE:- 26/09/2024
+*/
+
+public class Constants {
+    public static final String INVALID_HEXCHAR = "Invalid Hexadecimal character: ";
+    public static final String ENTER_FBINARY = "Enter first binary number: ";
+    public static final String ENTER_SBINARY = "Enter second binary number: ";
+    public static final String ENTER_FDECIMAL = "Enter first decimal number: ";
+    public static final String ENTER_SDECIMAL = "Enter second decimal number: ";
+    public static final String ENTER_FOCTAL = "Enter first octal number: ";
+    public static final String ENTER_SOCTAL = "Enter second octal number: ";
+    public static final String ENTER_FHEXADECIMAL = "Enter first hexadecimal number: ";
+    public static final String ENTER_SHEXADECIMAL = "Enter second hexadecimal number: ";
+    public static final String INVALID_HEXADECIMAL = "Invalid hexadecimal input. Please try again.";
+    public static final String DIVISION_NOT = "Division by zero is not allowed.";
+    public static final String R_BINARY = "Result in Binary: ";
+    public static final String R_DECIMAL = "Result in Decimal: ";
+    public static final String R_OCTAL = "Result in Octal: ";
+    public static final String R_HEXADECIMAL = "Result in Hexadecimal: ";
+    public static final String MENU = "\n--- Number System Menu ---";
+    public static final String B_TO_D = "1. Binary to Decimal";
+    public static final String B_TO_O = "2. Binary to Octal";
+    public static final String B_TO_H = "3. Binary to Hexadecimal";
+    public static final String D_TO_B = "4. Decimal to Binary";
+    public static final String D_TO_C = "5. Decimal to Octal";
+    public static final String D_TO_H = "6. Decimal to Hexadecimal";
+    public static final String O_TO_D = "7. Octal to Decimal";
+    public static final String O_TO_B = "8. Octal to Binary";
+    public static final String O_TO_H = "9. Octal to Hexadecimal";
+    public static final String H_TO_D = "10. Hexadecimal to Decimal";
+    public static final String H_TO_B = "11. Hexadecimal to Binary";
+    public static final String H_TO_O = "12. Hexadecimal to Octal";
+    public static final String ARITHMETIC_OP = "13. Arithmetic Operations";
+    public static final String EXIT  ="14. Exit";
+    public static final String ENTER_CHOICE  = "Enter your choice: ";
+    public static final String ENTER_B_NUM = "Enter Binary Number: ";
+    public static final String DECIMAL_VALUE = "Decimal Value: ";
+    public static final String OCTAL_VALUE = "Octal Value: ";
+    public static final String HEXADECIMAL_VALUE = "Hexadecimal Value: ";
+    public static final String BINARY_VALUE = "Binary Value: ";
+    public static final String ENTER_DECIMAL = "Enter Decimal Number: ";
+    public static final String ENTER_OCTAL = "Enter Octal Number: ";
+    public static final String ENTER_HEXADECIMAL = "Enter Hexadecimal Number: ";
+    public static final String CHOOSE_N_S = "\nChoose Number System:";
+    public static final String B = "1. Binary";
+    public static final String D = "2. Decimal";
+    public static final String O = "3. Octal";
+    public static final String H = "4. Hexadecimal";
+    public static final String CHOOSE_OPERATION = "\nChoose Operation:";
+    public static final String ADD = "1. Addition";
+    public static final String SUB = "2. Subtraction";
+    public static final String MULTI = "3. Multiplication";
+    public static final String DIV = "4. Division";
+    public static final String INVALID = "Invalid choice. Please try again.";
+
+}
+
